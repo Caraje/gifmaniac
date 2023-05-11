02bf600f191e5e3944cb72473f30d73a593ef0e8
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-11 19:47:19 +0200

    add colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        montserrat: ['montserrat', 'sans-serif']
+      },
+      colors: {
+        gifBlue: '#4C4C6D',
+        gifGreen: '#1B9C85',
+        gifWhite: '#E8F6EF',
+        gifBlack: '#333333',
+        gifyellow: '#FFE194'
+      }
+    }
   },
   plugins: []
 }
