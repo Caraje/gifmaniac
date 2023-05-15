1c5d81287ea1442e31d9846881ec5e9d9d0d6884
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 17:30:18 +0200

    create: categories data file

---
+++ src/db/categories.js
@@ -0,0 +1,23 @@
+export const categories = [
+  {
+    id: 'cinema',
+    name: 'Cine',
+    url: '/cinema',
+    img: '/static_img/indy.webp',
+    alt: 'Es una ilustracion creada por Dan Munford sobre Indiana Jones'
+  },
+  {
+    id: 'series',
+    name: 'Series',
+    url: '/series',
+    img: '/static_img/futurama.webp',
+    alt: 'Es una ilustracion creada por Dan Munford sobre Futurama'
+  },
+  {
+    id: 'videogames',
+    name: 'Videojuegos',
+    url: '/videogames',
+    img: '/static_img/mario.webp',
+    alt: 'Es una ilustracion creada por Dan Munford sobre Mario Bros'
+  }
+]
