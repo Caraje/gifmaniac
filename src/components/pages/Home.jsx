736ce0ee7d499c6853cdfbc800be1146d0d2d09e
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 13:54:26 +0200

    change name Home for HomePage

--- src/components/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-export const Home = () => {
-  return (
-    <div>Esta es la pantalla de inicio y seleccion de categoria</div>
-  )
-}
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gifBlue">
+      Esta es la pantalla de inicio y seleccion de categoria
+    </div>
+  )
+}
