d352595931788b483a5e6e1651ad2dfe7715efc1
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 17:31:45 +0200

    add navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { HomePage } from './components/pages/HomePage'
 import { GamePage } from './components/pages/GamePage'
+import { Footer } from './components/ui/Footer'
 
 const NotFound = () => <Navigate to="/" replace />
 function App () {
   return (
-  // <div className="w-screen h-screen flex justify-center items-center bg-gifBlue">
-      <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/cinema' element={<GamePage />}/>
-        <Route path='/series' element={<GamePage />}/>
-        <Route path='/videogames' element={<GamePage />}/>
-        <Route path='*' element={<NotFound />}/>
+  <div className="w-screen h-screen flex flex-col justify-center items-center">
 
-      </Routes>
-
-  // </div>
+    <a
+      href='/'
+      className='font-gifTitles text-6xl text-gifyellow p-2'
+    >
+      GifManiac
+    </a>
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='/cinema' element={<GamePage />}/>
+      <Route path='/series' element={<GamePage />}/>
+      <Route path='/videogames' element={<GamePage />}/>
+      <Route path='*' element={<NotFound />}/>
+    </Routes>
+    <Footer />
+   </div>
   )
 }
 
