aaf6022591b1c0fe4b12092dae6b1b88f3d3aa5f
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 18:08:30 +0200

    fix: orthographic

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -19,8 +19,7 @@ export const HomePage = () => {
         }
         </section>
         <div className='font-montserrat max-w-[74ch] text-center text-gifWhite text-lg'>
-          <p>Selecciona una de las categorias disponibles y adivina cual es el titulo de la pelicula, serie o videojuego mediante los gifs animados que te saldran en pantalla.</p>
-          <p>Los gifs son generados de manera aleatoria usando la API de <a href='https://giphy.com/' target='_blank' rel='noreferrer' className='font-bold text-gifyellow'>giphy.com</a>, asi que ten en cuenta que puede que no sean del todo exactas.</p>
+          <p>Selecciona una de las categorías disponibles y adivina cuál es el título de la película, serie o videojuego mediante los gifs animados que aparecerán en pantalla. Los gifs son generados de manera aleatoria utilizando la API de <a href='https://giphy.com/' target='_blank' rel='noreferrer' className='font-bold text-gifyellow'>giphy.com</a>, así que ten en cuenta que es posible que no sean completamente exactos.</p>
         </div>
     </main>
   )
