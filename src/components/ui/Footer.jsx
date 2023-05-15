f354f1b073bf0c87ac5501170c9218eb4cb03b71
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 18:08:37 +0200

    add Logo

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,9 +1,10 @@
+
 export const Footer = () => {
   return (
-    <footer className="w-full max-w-[1000px] flex items-center justify-between p-2">
-      <section className="bg-red-900">
-        carlos ajenjo
-        redes
+    <footer className="w-full max-w-[1000px] flex items-center justify-between " >
+      <section className="max-w-md">
+        <img src="/static_img/LogoCarlos.webp" width={932} height={226}/>
+        {/* redes */}
       </section>
       <p className="font-montserrat max-w-[30ch] text-right">El copyright de las ilustraciones pertenece a <a href="https://www.dan-mumford.com/" target="_blank" rel="noreferrer" className=" font-montserrat font-bold text-gifyellow">Dan Mumford</a></p>
     </footer>
