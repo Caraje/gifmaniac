90bc967f8a84999491150b0126992a1069a40682
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 13:53:54 +0200

    change name game for GamePage

--- src/components/pages/Game.jsx
+++
@@ -1,5 +0,0 @@
-export const Game = () => {
-  return (
-    <div>Esta es la pantalla Juego de cada categoria</div>
-  )
-}
---
+++ src/components/pages/GamePage.jsx
@@ -0,0 +1,10 @@
+export const GamePage = () => {
+  console.log(window.location.pathname)
+  const pathName = window.location.pathname
+  const data = pathName === '/cinema' ? 'Cine' : pathName === '/series' ? 'Series' : 'Videojuegos'
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gifBlue">
+      Esta es la pantalla de juego de {data}
+    </div>
+  )
+}
