b2fb6cc7453b84c52864a9121cc055123ed663da
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 13:53:34 +0200

    configure: react Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { HomePage } from './components/pages/HomePage'
+import { GamePage } from './components/pages/GamePage'
 
+const NotFound = () => <Navigate to="/" replace />
 function App () {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gifBlue">
+  // <div className="w-screen h-screen flex justify-center items-center bg-gifBlue">
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/cinema' element={<GamePage />}/>
+        <Route path='/series' element={<GamePage />}/>
+        <Route path='/videogames' element={<GamePage />}/>
+        <Route path='*' element={<NotFound />}/>
 
-    </div>
+      </Routes>
+
+  // </div>
   )
 }
 
