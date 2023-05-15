3b9aa28d9e39e2bfef01e7642906560bdf9052ec
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-15 17:30:28 +0200

    create Footer component

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full max-w-[1000px] flex items-center justify-between p-2">
+      <section className="bg-red-900">
+        carlos ajenjo
+        redes
+      </section>
+      <p className="font-montserrat max-w-[30ch] text-right">El copyright de las ilustraciones pertenece a <a href="https://www.dan-mumford.com/" target="_blank" rel="noreferrer" className=" font-montserrat font-bold text-gifyellow">Dan Mumford</a></p>
+    </footer>
+  )
+}
