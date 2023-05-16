d3d4f41aa66622240de4efa15e4f3bfb98c71ad2
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 12:53:06 +0200

    add file witch buttons data

---
+++ src/db/buttons.jsx
@@ -0,0 +1,26 @@
+export const buttonsGame = [
+  {
+    id: 1,
+    name: 'Rendirse',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4.207 12.793-1.414 1.414L12 13.414l-2.793 2.793-1.414-1.414L10.586 12 7.793 9.207l1.414-1.414L12 10.586l2.793-2.793 1.414 1.414L13.414 12l2.793 2.793z"></path></svg>,
+    href: null
+  },
+  {
+    id: 2,
+    name: 'Siguiente Titulo',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4 14h-2v-4l-6 4V8l6 4V8h2v8z"></path></svg>,
+    href: null
+  },
+  {
+    id: 3,
+    name: 'Volver',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="currentColor" ><path d="M12.74 2.32a1 1 0 0 0-1.48 0l-9 10A1 1 0 0 0 3 14h2v7a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 1 1 0 0 0-.26-.68z"></path></svg>,
+    href: '/'
+  },
+  {
+    id: 4,
+    name: 'Siguiente Gif',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 15v-4H7v-2h5V7l5 5-5 5z"></path></svg>,
+    href: null
+  }
+]
