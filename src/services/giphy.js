595bfab13109177f58d00aabab42dabc18a80e1f
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 18:06:30 +0200

    add Environment Variables

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,5 +1,8 @@
+
+const GIPHY_KEY = import.meta.env.VITE_APY_KEY_GIPHY
+
 export const getGifs = async (title) => {
-  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${title}&limit=100&api_key=PkAyZJEOlm1sq4UdOtl0doCklSwGOELe`)
+  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${title}&limit=100&api_key=${GIPHY_KEY}`)
   const { data } = await res.json()
   const gifList = data.map(gif => {
     return {
