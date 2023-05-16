212c2668042afb008c2e3dab23970a8e7c57b066
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 17:48:58 +0200

    change limit

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,5 +1,5 @@
 export const getGifs = async (title) => {
-  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${title}&limit=10&api_key=PkAyZJEOlm1sq4UdOtl0doCklSwGOELe`)
+  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${title}&limit=100&api_key=PkAyZJEOlm1sq4UdOtl0doCklSwGOELe`)
   const { data } = await res.json()
   const gifList = data.map(gif => {
     return {
