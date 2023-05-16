64e810c11fc27db6bc475723d9af9a782258758a
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 13:21:44 +0200

    add responsive

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,12 +1,12 @@
 
 export const Footer = () => {
   return (
-    <footer className="w-full max-w-[1000px] flex items-center justify-between " >
+    <footer className="w-full max-w-xs sm:max-w-[1000px] flex flex-col sm:flex-row items-center justify-between mt-10 mb-4" >
       <section className="max-w-md">
         <img src="/static_img/LogoCarlos.webp" width={932} height={226}/>
         {/* redes */}
       </section>
-      <p className="font-montserrat max-w-[30ch] text-right">El copyright de las ilustraciones pertenece a <a href="https://www.dan-mumford.com/" target="_blank" rel="noreferrer" className=" font-montserrat font-bold text-gifyellow">Dan Mumford</a></p>
+      <p className="font-montserrat max-w-[30ch] text-center sm:text-right">El copyright de las ilustraciones pertenece a <a href="https://www.dan-mumford.com/" target="_blank" rel="noreferrer" className=" font-montserrat font-bold text-gifyellow">Dan Mumford</a></p>
     </footer>
   )
 }
