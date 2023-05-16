58d40c4809f9e3358902bd07e329c4c3324c8456
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 14:23:20 +0200

    create services giphy

---
+++ src/services/giphy.js
@@ -0,0 +1,11 @@
+export const getGifs = async (title) => {
+  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${title}&limit=10&api_key=PkAyZJEOlm1sq4UdOtl0doCklSwGOELe`)
+  const { data } = await res.json()
+  const gifList = data.map(gif => {
+    return {
+      id: gif.id,
+      url: gif.images?.original.url
+    }
+  })
+  return gifList
+}
