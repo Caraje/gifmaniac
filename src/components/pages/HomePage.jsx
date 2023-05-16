52a737f563d806c1651fb9c2a932c2825236bae7
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 13:21:36 +0200

    add responsive

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import { CategoryCard } from '../ui/CategoryCard'
 
 export const HomePage = () => {
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center gap-10 bg-gifBlue ">
-      <h1 className='font-gifTitles text-5xl'>Seleccione una categoria</h1>
-      <section className='flex justify-center items-center gap-10'>
+    <main className="flex flex-col justify-center items-center gap-10 bg-gifBlue ">
+      <h1 className='font-gifTitles text-2xl sm:text-5xl'>Seleccione una categoria</h1>
+      <section className='flex flex-col sm:flex-row justify-center items-center gap-10'>
         {
           categories.map(cat => (
             <CategoryCard
@@ -18,7 +18,7 @@ export const HomePage = () => {
           ))
         }
         </section>
-        <div className='font-montserrat max-w-[74ch] text-center text-gifWhite text-lg'>
+        <div className='font-montserrat max-w-xs sm:max-w-[74ch] text-center text-gifWhite text-lg'>
           <p>Selecciona una de las categorías disponibles y adivina cuál es el título de la película, serie o videojuego mediante los gifs animados que aparecerán en pantalla. Los gifs son generados de manera aleatoria utilizando la API de <a href='https://giphy.com/' target='_blank' rel='noreferrer' className='font-bold text-gifyellow'>giphy.com</a>, así que ten en cuenta que es posible que no sean completamente exactos.</p>
         </div>
     </main>
