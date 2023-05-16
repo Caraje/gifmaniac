dad95e35ea2c304488e582dcf4b99c145bee890e
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-05-16 13:21:26 +0200

    add responsive

--- src/components/pages/GamePage.jsx
+++ src/components/pages/GamePage.jsx
@@ -6,10 +6,10 @@ export const GamePage = () => {
   const category = pathName === '/cinema' ? 'Cine' : pathName === '/series' ? 'Series' : 'Videojuegos'
 
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center gap-10 bg-gifBlue p-4">
+    <main className="w-screen  flex flex-col justify-center items-center gap-10 bg-gifBlue p-4">
       <section className='w-full max-w-[1000px] flex flex-col gap-4'>
-        <header className='flex  justify-between items-end'>
-          <h1 className='font-gifTitles text-7xl text-gifyellow'>{category}</h1>
+        <header className='flex flex-col sm:flex-row justify-between items-center sm:items-end'>
+          <h1 className='font-gifTitles text-5xl sm:text-7xl text-gifyellow'>{category}</h1>
           <OptionsGame />
         </header>
         <footer className='w-full flex flex-col gap-4 justify-center items-center'>
@@ -17,9 +17,10 @@ export const GamePage = () => {
             <img className='w-full max-h-[600px] object-contain z-10' src='/static_img/mario3.gif'/>
             <img className=' absolute w-full max-h-[600px] object-cover blur-lg' src='/static_img/mario3.gif'/>
           </div>
-          <form className='w-full flex gap-4'>
-            <input className='w-4/5 p-4 rounded-xl border-2 border-gifyellow text-gifBlack font-montserrat font-medium text-xl' />
-            <button className='w-1/5 bg-gifGreen rounded-xl border-2 border-gifGreen font-montserrat font-bold text-xl hover:scale-105 hover:brightness-90 transition-all'>Enviar</button>
+          <h2 className='font-montserrat font-semibold text-gifyellow text-lg '>Adivina el {category} con el Gif</h2>
+          <form className='w-full flex flex-col sm:flex-row gap-4'>
+            <input className='w-full sm:w-4/5 p-4 rounded-xl border-2 border-gifyellow text-gifBlack font-montserrat font-medium text-xl' />
+            <button className='w-full sm:w-1/5 bg-gifGreen rounded-xl border-2 border-gifGreen font-montserrat font-bold text-xl hover:scale-105 hover:brightness-90 transition-all'>Enviar</button>
           </form>
         </footer>
 
